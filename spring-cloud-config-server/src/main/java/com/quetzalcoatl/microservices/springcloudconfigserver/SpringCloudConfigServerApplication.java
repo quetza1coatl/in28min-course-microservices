df407f28ae80b401bd926b5505661251ab2c0c53
Quetzalcoatl <www.quetzalcoatl@gmail.com>
2022-04-24 18:15:28 +0300

    add external configuration

--- spring-cloud-config-server/src/main/java/com/quetzalcoatl/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/com/quetzalcoatl/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -3,6 +3,7 @@ package com.quetzalcoatl.microservices.springcloudconfigserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableConfigServer
 @SpringBootApplication
 public class SpringCloudConfigServerApplication {
 
