d6e226bc3b3a1380f91dc6b8460c710b7578ef1f
Quetzalcoatl <www.quetzalcoatl@gmail.com>
2022-05-04 21:58:26 +0300

    add h2 to currency exchange service

--- currency-exchange-service/src/main/java/com/quetzalcoatl/microservices/currencyexchangeservice/CurrencyExchange.java
+++ currency-exchange-service/src/main/java/com/quetzalcoatl/microservices/currencyexchangeservice/CurrencyExchange.java
@@ -1,10 +1,17 @@
 package com.quetzalcoatl.microservices.currencyexchangeservice;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
+@Entity
 public class CurrencyExchange {
+    @Id
     private Integer id;
+    @Column(name = "currency_from")
     private String from;
+    @Column(name = "currency_to")
     private String to;
     private BigDecimal conversionMultiple;
     private String environment;
---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(10001,'USD','BLR',2,'');
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(10002,'EUR','BLR',3,'');
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(10003,'BLR','INR',40,'');
