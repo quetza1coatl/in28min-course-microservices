2b3b5e1262a3e40fd5774707cbd2163356d4c366
Quetzalcoatl <www.quetzalcoatl@gmail.com>
2022-05-04 22:04:49 +0300

    add hardcoded endpoint for currency conversion service

---
+++ currency-conversion-service/src/main/java/com/quetzalcoatl/microservices/currencyconversionservice/CurrencyConversion.java
@@ -0,0 +1,89 @@
+package com.quetzalcoatl.microservices.currencyconversionservice;
+
+import java.math.BigDecimal;
+
+public class CurrencyConversion {
+    private Integer id;
+    private String from;
+    private String to;
+    private BigDecimal quantity;
+    private BigDecimal conversionMultiple;
+    private BigDecimal totalCalculatedAmount;
+    private String environment;
+
+    public CurrencyConversion() {
+
+    }
+
+
+    public CurrencyConversion(
+            Integer id, String from, String to,
+            BigDecimal quantity, BigDecimal conversionMultiple,
+            BigDecimal totalCalculatedAmount, String environment
+    ) {
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.quantity = quantity;
+        this.conversionMultiple = conversionMultiple;
+        this.totalCalculatedAmount = totalCalculatedAmount;
+        this.environment = environment;
+    }
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public BigDecimal getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(BigDecimal quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getConversionMultiple() {
+        return conversionMultiple;
+    }
+
+    public void setConversionMultiple(BigDecimal conversionMultiple) {
+        this.conversionMultiple = conversionMultiple;
+    }
+
+    public BigDecimal getTotalCalculatedAmount() {
+        return totalCalculatedAmount;
+    }
+
+    public void setTotalCalculatedAmount(BigDecimal totalCalculatedAmount) {
+        this.totalCalculatedAmount = totalCalculatedAmount;
+    }
+
+    public String getEnvironment() {
+        return environment;
+    }
+
+    public void setEnvironment(String environment) {
+        this.environment = environment;
+    }
+}
